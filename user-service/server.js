b3f012be027da2f1ef0397a68d7d508ae2f7ecab
sathya <leosathya10@gmail.com>
2026-01-10 21:06:36 +0530

    add Basic server with mongoDB (Docker) connection

---
+++ user-service/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/users");
+    console.log("DB Connection Sucessful");
+  } catch (error) {
+    console.log("DB Connection Failed");
+  }
+};
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port :: 3000`);
+});
+
+connectDB();
